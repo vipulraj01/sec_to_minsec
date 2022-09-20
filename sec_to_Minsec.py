1f2f00ddad7a3bdf8018290e35bbae366bfbeb80
Vipul Raj <38418118+vipulraj01@users.noreply.github.com>
2022-09-20 20:20:30 +0530

    Update sec_to_Minsec.py

--- sec_to_Minsec.py
+++ sec_to_Minsec.py
@@ -1,3 +1,6 @@
+# Converts seconds to seconds.
 seconds = int(input())
+# Converts minutes and seconds to seconds.
 minutes, seconds = divmod(seconds, 60)
+# Prints a human - readable representation of a time.
 print("%d M : %0d S"%(minutes,seconds))
